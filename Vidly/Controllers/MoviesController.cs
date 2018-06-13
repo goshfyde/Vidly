a1f6de8a0ddd059ca663a862d968207461ace83c
Hayden Bush <hayden.p.bush@gmail.com>
2018-06-13 14:32:04 -0400

    section 2 lecture 11

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -13,10 +13,24 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!"};
-            //return View(movie);
-            //return HttpNotFound();
-            //return new EmptyResult();
-            return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+            return View(movie);
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            return Content("Id=" + Id);
+        }
+
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
