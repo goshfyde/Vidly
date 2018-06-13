5afc433606f163bcf119484ecb4a543161b119d9
Hayden Bush <hayden.p.bush@gmail.com>
2018-06-13 14:21:46 -0400

    Section 2 lecture 10

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -13,7 +13,10 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!"};
-            return View(movie);
+            //return View(movie);
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
         }
     }
 }
