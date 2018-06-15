70828eabc04117400d2065c147ea31eae1d1da7e
Hayden Bush <hayden.p.bush@gmail.com>
2018-06-15 16:58:00 -0400

    Section 2 Lecture 19

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -36,15 +36,15 @@ namespace Vidly.Controllers
         }
 
         //movies
-        public ActionResult Index(int? pageIndex, string sortBy)
+        public ActionResult Index()
         {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
+            var movieList = new List<Movie>
+            {
+                new Movie(){Id = 1, Name = "Shrek"},
+                new Movie(){Id = 2, Name = "Wall-e"}
+            };
 
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            return View(movieList);
         }
 
         [Route("movies/released/{year}/{month:regex(\\d{1}|d{2}):range(1,12)}")]
