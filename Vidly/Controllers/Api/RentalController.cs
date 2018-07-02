4ca03bb1ae5561c7edc2a46b569b17b75b35943c
Hayden Bush <hayden.p.bush@gmail.com>
2018-07-02 12:41:05 -0400

    Section 10 Lecture 114

---
+++ Vidly/Controllers/Api/RentalController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class RentalController : ApiController
+    {
+        public ApplicationDbContext _context { get; set; }
+
+        public RentalController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateRental(int customerId, int movieId)
+        {
+            return Ok();
+        }
+    }
+}
