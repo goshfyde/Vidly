b5852d8909b6fdcfbcf41950318cf5485f4a2dbb
Hayden Bush <hayden.p.bush@gmail.com>
2018-07-02 14:07:50 -0400

    Section 10 Lecture 119

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -22,9 +22,14 @@ namespace Vidly.Controllers.Api
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
             var customerInDb = _context.Customers.Single(c => c.Id == newRental.CustomerId);
-            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id));
+
+            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id)).ToList();
+
             foreach (var movie in movies)
             {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available");
+                movie.NumberAvailable--;
                 var rental = new Rental
                     {
                         Customer = customerInDb,
@@ -32,7 +37,6 @@ namespace Vidly.Controllers.Api
                         DateRented = DateTime.Now
                     };
                 _context.Rentals.Add(rental);
-                movie.NumberAvailable--;
             }
 
             _context.SaveChanges();
